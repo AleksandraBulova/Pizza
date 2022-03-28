24669f26f17b6fed6410695bf8a4aeb9d3c5fc4d
AleksandraBulova <aliaksandra.ihnatsenia@gmail.com>
2022-03-28 23:44:03 +0400

    implementation all pizzes

--- data/pizzaSet.js
+++ data/pizzaSet.js
@@ -1,10 +1,11 @@
-function Pizza(name, img, dough) {
+function Pizza(name, img, id, dough) {
   this.name = name;
   this.img = img;
-  this.dough = dough;
+  this.id = id;
+  this.dough = dough
 }
 
-let cheeseburger = new Pizza('Чизбургер-пицца', '/assets/images/cheeseburger_pizza.png', {
+let cheeseburger = new Pizza('Чизбургер-пицца', '/assets/images/cheeseburger_pizza.png', 1, {
     thin: {
       26: 300,
       30: 340,
@@ -18,6 +19,104 @@ let cheeseburger = new Pizza('Чизбургер-пицца', '/assets/images/ch
   }
 )
 
-let pizzaSet = [cheeseburger];
+let cheese = new Pizza('Сырная', '/assets/images/cheese.png', 2, {
+    thin: {
+      26: 300,
+      30: 340,
+      40: 380
+    },
+    traditional: {
+      26: 320,
+      30: 360,
+      40: 400
+    }
+  }
+)
+
+let shrimps = new Pizza('Креветки по-азиатски', '/assets/images/shrimps.png', 3, {
+    thin: {
+      26: 300,
+      30: 340,
+      40: 380
+    },
+    traditional: {
+      26: 320,
+      30: 360,
+      40: 400
+    }
+  }
+)
+
+let cheeseСhicken = new Pizza('Сырныйцыпленок', '/assets/images/cheese_chicken.png', 4, {
+    thin: {
+      26: 300,
+      30: 340,
+      40: 380
+    },
+    traditional: {
+      26: 320,
+      30: 360,
+      40: 400
+    }
+  }
+)
+
+let cheeseburger1 = new Pizza('Чизбургер-пицца', '/assets/images/cheeseburger_pizza.png', 5, {
+    thin: {
+      26: 300,
+      30: 340,
+      40: 380
+    },
+    traditional: {
+      26: 320,
+      30: 360,
+      40: 400
+    }
+  }
+)
+
+let cheese1 = new Pizza('Сырная', '/assets/images/cheese.png', 6, {
+    thin: {
+      26: 300,
+      30: 340,
+      40: 380
+    },
+    traditional: {
+      26: 320,
+      30: 360,
+      40: 400
+    }
+  }
+)
+
+let shrimps1 = new Pizza('Креветки по-азиатски', '/assets/images/shrimps.png', 7, {
+    thin: {
+      26: 300,
+      30: 340,
+      40: 380
+    },
+    traditional: {
+      26: 320,
+      30: 360,
+      40: 400
+    }
+  }
+)
+
+let cheeseСhicken1 = new Pizza('Сырныйцыпленок', '/assets/images/cheese_chicken.png', 8, {
+    thin: {
+      26: 300,
+      30: 340,
+      40: 380
+    },
+    traditional: {
+      26: 320,
+      30: 360,
+      40: 400
+    }
+  }
+)
+
+let pizzaSet = [cheeseburger, cheese, shrimps, cheeseСhicken, cheeseburger1, cheese1, shrimps1, cheeseСhicken1];
 
 export default pizzaSet;
--- script.js
+++ script.js
@@ -1,124 +1,121 @@
 import pizzaSet from './data/pizzaSet.js';
 
+for (let i = 0; i < pizzaSet.length; i++){
 
-let pizza = document.createElement('div');
-pizza.className = 'pizza';
+  let pizza = document.createElement('div');
+  pizza.className = 'pizza';
 
-let pizzaImg = document.createElement('img');
-pizzaImg.src = '/assets/images/cheeseburger_pizza.png';
-pizzaImg.alt = 'pizza';
-pizza.append(pizzaImg);
+  let pizzaImg = document.createElement('img');
+  pizzaImg.src = pizzaSet[i].img;
+  pizzaImg.alt = 'pizza';
+  pizza.append(pizzaImg);
 
-let namePizza = document.createElement('h3');
-pizzaImg.after(namePizza);
-namePizza.innerHTML = 'Чизбургер-пицца';
+  let namePizza = document.createElement('h3');
+  pizzaImg.after(namePizza);
+  namePizza.innerHTML = pizzaSet[i].name;
 
-let pizzaOptions = document.createElement('div');
-pizzaOptions.className = 'options_pizza';
-namePizza.after(pizzaOptions);
+  let pizzaOptions = document.createElement('form');
+  pizzaOptions.className = 'options_pizza';
+  namePizza.after(pizzaOptions);
 
-let pizzaDough = document.createElement('div');
-pizzaDough.className = 'dough_pizza';
-pizzaOptions.append(pizzaDough);
+  let pizzaDough = document.createElement('div');
+  pizzaDough.className = 'dough_pizza';
+  pizzaOptions.append(pizzaDough);
 
-let thinPizza = document.createElement('input');
-thinPizza.type = 'radio';
-thinPizza.id = 'radio-dough-1';
-thinPizza.name = 'radios-dough';
-thinPizza.value = 'тонкое';
-thinPizza.checked = true;
-pizzaDough.append(thinPizza);
+  let thinPizza = document.createElement('input');
+  thinPizza.type = 'radio';
+  thinPizza.id = `radio-dough-${pizzaSet[i].id}-1`;
+  thinPizza.name = 'radios-dough';
+  thinPizza.value = 'тонкое';
+  thinPizza.checked = true;
+  pizzaDough.append(thinPizza);
 
+  let thinDough = document.createElement('label');
+  thinDough.htmlFor = `radio-dough-${pizzaSet[i].id}-1`;
+  thinPizza.after(thinDough);
+  thinDough.innerHTML = 'тонкое';
 
-let thinDough = document.createElement('label');
-thinDough.htmlFor = 'radio-dough-1';
-thinPizza.after(thinDough);
-thinDough.innerHTML = 'тонкое';
+  let traditionalPizza = document.createElement('input');
+  traditionalPizza.type = 'radio';
+  traditionalPizza.id = `radio-dough-${pizzaSet[i].id}-2`;
+  traditionalPizza.name = 'radios-dough';
+  traditionalPizza.value = 'традиционное';
+  thinDough.after(traditionalPizza);
 
-let traditionalPizza = document.createElement('input');
-traditionalPizza.type = 'radio';
-traditionalPizza.id = 'radio-dough-2';
-traditionalPizza.name = 'radios-dough';
-traditionalPizza.value = 'традиционное';
-thinDough.after(traditionalPizza);
+  let traditionalDough = document.createElement('label');
+  traditionalDough.htmlFor = `radio-dough-${pizzaSet[i].id}-2`;
+  traditionalPizza.after(traditionalDough);
+  traditionalDough.innerHTML = 'традиционное';
 
+  let pizzaSize = document.createElement('div');
+  pizzaSize.className = 'size_pizza';
+  pizzaDough.after(pizzaSize);
 
-let traditionalDough = document.createElement('label');
-traditionalDough.htmlFor = 'radio-dough-2';
-traditionalPizza.after(traditionalDough);
-traditionalDough.innerHTML = 'традиционное';
+  let littlePizza = document.createElement('input');
+  littlePizza.type = 'radio';
+  littlePizza.id = `radios-size-${pizzaSet[i].id}-1`;
+  littlePizza.name = 'radios-size';
+  littlePizza.value = '26 см.';
+  littlePizza.checked = true;
+  pizzaSize.append(littlePizza);
 
-let pizzaSize = document.createElement('div');
-pizzaSize.className = 'size_pizza';
-pizzaDough.after(pizzaSize);
+  let littleSize = document.createElement('label');
+  littleSize.htmlFor = `radios-size-${pizzaSet[i].id}-1`;
+  littlePizza.after(littleSize);
+  littleSize.innerHTML = '26 см.';
 
-let littlePizza = document.createElement('input');
-littlePizza.type = 'radio';
-littlePizza.id = 'radios-size-1';
-littlePizza.name = 'radios-size';
-littlePizza.value = '26 см.';
-littlePizza.checked = true;
-pizzaSize.append(littlePizza);
+  let middlePizza = document.createElement('input');
+  middlePizza.type = 'radio';
+  middlePizza.id = `radios-size-${pizzaSet[i].id}-2`;
+  middlePizza.name = 'radios-size';
+  middlePizza.value = '30 см.';
+  littleSize.after(middlePizza);
 
-let littleSize = document.createElement('label');
-littleSize.htmlFor = 'radios-size-1';
-littlePizza.after(littleSize);
-littleSize.innerHTML = '26 см.';
+  let middleSize = document.createElement('label');
+  middleSize.htmlFor = `radios-size-${pizzaSet[i].id}-2`;
+  middlePizza.after(middleSize);
+  middleSize.innerHTML = '30 см.';
 
-let middlePizza = document.createElement('input');
-middlePizza.type = 'radio';
-middlePizza.id = 'radios-size-2';
-middlePizza.name = 'radios-size';
-middlePizza.value = '30 см.';
-littleSize.after(middlePizza);
+  let bigPizza = document.createElement('input');
+  bigPizza.type = 'radio';
+  bigPizza.id = `radios-size-${pizzaSet[i].id}-3`;
+  bigPizza.name = 'radios-size';
+  bigPizza.value = '40 см.';
+  middleSize.after(bigPizza);
 
-let middleSize = document.createElement('label');
-middleSize.htmlFor = 'radios-size-2';
-middlePizza.after(middleSize);
-middleSize.innerHTML = '30 см.';
+  let bigSize = document.createElement('label');
+  bigSize.htmlFor = `radios-size-${pizzaSet[i].id}-3`;
+  bigPizza.after(bigSize);
+  bigSize.innerHTML = '40 см.';
 
-let bigPizza = document.createElement('input');
-bigPizza.type = 'radio';
-bigPizza.id = 'radios-size-3';
-bigPizza.name = 'radios-size';
-bigPizza.value = '40 см.';
-middleSize.after(bigPizza);
+  let pizzaPriceAddition = document.createElement('div');
+  pizzaPriceAddition.className = 'price_addition_pizza';
+  pizzaOptions.after(pizzaPriceAddition);
 
-let bigSize = document.createElement('label');
-bigSize.htmlFor = 'radios-size-3';
-bigPizza.after(bigSize);
-bigSize.innerHTML = '40 см.';
+  let pizzaPrice = document.createElement('div');
+  pizzaPrice.className = 'price_pizza';
+  pizzaPriceAddition.append(pizzaPrice);
 
-let pizzaPriceAddition = document.createElement('div');
-pizzaPriceAddition.className = 'price_addition_pizza';
-pizzaOptions.after(pizzaPriceAddition);
+  let pizzaText = document.createElement('div');
+  pizzaPrice.append(pizzaText);
+  pizzaText.innerHTML = 'от';
 
-let pizzaPrice = document.createElement('div');
-pizzaPrice.className = 'price_pizza';
-pizzaPriceAddition.append(pizzaPrice);
+  let price = document.createElement('div');
+  price.className = 'price';
+  pizzaText.after(price);
+  price.innerHTML = '300 ₽';
 
-let pizzaText = document.createElement('div');
-pizzaPrice.append(pizzaText);
-pizzaText.innerHTML = 'от';
+  let buttonPizzaAdd = document.createElement('button');
+  pizzaPrice.after(buttonPizzaAdd);
 
-let price = document.createElement('div');
-price.className = 'price';
-pizzaText.after(price);
-price.innerHTML = '395 ₽';
+  let add = document.createElement('div');
+  buttonPizzaAdd.append(add);
+  add.innerHTML = '+';
 
-let buttonPizzaAdd = document.createElement('button');
-pizzaPrice.after(buttonPizzaAdd);
+  let pizzaAdd = document.createElement('div');
+  add.after(pizzaAdd);
+  pizzaAdd.innerHTML = 'Добавить';
 
-let add = document.createElement('div');
-buttonPizzaAdd.append(add);
-add.innerHTML = '+';
+  document.querySelector('.list_pizza').append(pizza);
 
-let pizzaAdd = document.createElement('div');
-add.after(pizzaAdd);
-pizzaAdd.innerHTML = 'Добавить';
-
-console.log(pizza);
-document.querySelector('.list_pizza').append(pizza);
-
-
-
+}
